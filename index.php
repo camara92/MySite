1a9fc5e8324dd45db644a20156bc77cfc36a55ad
camara92 <88945535+camara92@users.noreply.github.com>
2022-05-22 20:08:19 +0200

    commit 1

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+include("partials/header.php"); ?>
+
+<h1>Mon portefolio en PHP</h1>
+<?php
+
+include("partials/Footer.php");
+?>
---
+++ partials/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CAMARA Daouda </title>
+</head>
+
+<body>
+    <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand">Mon portefolio en PHP </a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
